bd942c3f4e8cff7e316fe779e8a856c13418030d
googoochadwick <arush.anand.singh.3121@gmail.com>
2026-08-28 11:05:52 +0530

    lesson 3 fix

--- main.m
+++ main.m
@@ -2,9 +2,9 @@ n = input("");
 if mod(n, 15) == 0
     disp('FizzBuzz')
 elseif mod(n, 5) == 0
-    disp('Fizz')
-elseif mod(n, 3) == 0
     disp('Buzz')
+elseif mod(n, 3) == 0
+    disp('Fizz')
 else
     disp('Error')
 endif
