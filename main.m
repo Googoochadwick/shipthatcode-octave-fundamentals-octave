55c8543cad683e4a72f3f28f6b57b863b06f9316
googoochadwick <arush.anand.singh.3121@gmail.com>
2026-08-28 10:51:39 +0530

    lesson 2

--- main.m
+++ main.m
@@ -1,2 +1,6 @@
-% disp(...)
-disp("Hello, Octave!")
+n = input("");
+line = input("", "s");
+v = str2num(line);
+printf("sum: %d\n", sum(n) )
+printf("mean: %.3f\n", mean (n))
+printf("max: %d\n", max(n))
